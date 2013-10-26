68a60551b35a950dd2066d976e083c6d471a5570
grant <grantelliottgeorge@gmail.com>
2013-10-26 00:59:53 -0400

    add to admin stie[A

--- thermostats/views.py
+++ thermostats/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
 
+from thermostats.models import Thermostat
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the thermostat index.")
+	thermostat_list = Thermostats.objects.order_by('name')[:5]
+	output = ', '.join([t.name for t in thermostat_list])
+	return HttpResponse(output)
 
 def detail(request, thermostat_id):
-    return HttpResponse("You're looking at thermostats %s." % thermostat_id)
+    return HttpResponse("You're looking at thermostat %s." % thermostat_id)
 
 def settings(request, thermostat_id):
     return HttpResponse("You're looking at the settings of thermostat %s." % thermostat_id)
 
 def temperature(request, thermostat_id):
-    return HttpResponse("You're temperature information on thermostat %s." % thermostat_id)
+    return HttpResponse("You're looking at the temperature information of thermostat %s." % thermostat_id)
