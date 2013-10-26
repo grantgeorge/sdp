19e0f44777f9a83ce99068508ecf8b209f69ae34
grant <grantelliottgeorge@gmail.com>
2013-10-26 00:28:46 -0400

    add to admin stie

--- thermostats/admin.py
+++ thermostats/admin.py
@@ -3,14 +3,19 @@ from thermostats.models import BaseControl, Thermostat
 
 class BaseControlAdmin(admin.ModelAdmin):
 	fieldsets = [
-		(None, {'fields': ['account_id', 'basecontrol_id', 'name']}),
-		('Temperature Information', {'fields':['current_temperature', 'setpoint_temperature', 'setback_temperature', 'status']}),
+		('Settings', {'fields': ['account_id', 'basecontrol_id', 'name']}),
+		('Temperature Control', {'fields':['current_temperature', 'setpoint_temperature', 'setback_temperature', 'status']}),
 	]
 	list_display = ('name', 'account_id', 'basecontrol_id')
 	search_fields = ['name']
 
 class ThermostatAdmin(admin.ModelAdmin):
-	fields = ['base_control', 'account_id', 'thermostat_id', 'name', 'current_temperature', 'setpoint_temperature', 'setback_temperature', 'status']
+	fieldsets = [
+		('Settings', {'fields': ['basecontrol_id','account_id', 'thermostat_id', 'name']}),
+		('Temperature Control', {'fields':['current_temperature', 'setpoint_temperature', 'setback_temperature', 'status']}),
+	]
+	list_display = ('base_control', 'name', 'account_id', 'basecontrol_id')
+	search_fields = ['name']
 
 admin.site.register(BaseControl, BaseControlAdmin)
 admin.site.register(Thermostat, ThermostatAdmin)
