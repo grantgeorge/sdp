1731b08ede95886a8fbbf41b6d339f32b1f94db8
grant <grantelliottgeorge@gmail.com>
2013-10-26 00:17:07 -0400

    add to admin stie

--- thermostats/admin.py
+++ thermostats/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from thermostats.models import BaseControl, Thermostat
 
 class BaseControlAdmin(admin.ModelAdmin):
-	fields = ['account_id', 'basecontrol_id', 'name', 'current_temperature', 'setpoint_temperature', 'setback_temperature', 'status']
+	fieldsets = [
+		(None, {'fields': ['account_id', 'basecontrol_id', 'name']}),
+		('Temperature Information', {'fields':['current_temperature', 'setpoint_temperature', 'setback_temperature', 'status']}),
+	]
 
 class ThermostatAdmin(admin.ModelAdmin):
 	fields = ['base_control', 'account_id', 'thermostat_id', 'name', 'current_temperature', 'setpoint_temperature', 'setback_temperature', 'status']
