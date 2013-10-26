1524aa287b5da5160f22697d36dd2a3fc7026880
grant <grantelliottgeorge@gmail.com>
2013-10-26 00:51:22 -0400

    add to admin stie[A

--- thermostats/urls.py
+++ thermostats/urls.py
@@ -3,5 +3,11 @@ from django.conf.urls import patterns, url
 from thermostats import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    # ex: /thermostats/5/
+    url(r'^(?P<thermostat_id>\d+)/$', views.detail, name='detail'),
+    # ex: /thermostats/5/settings/
+    url(r'^(?P<thermostat_id>\d+)/settings/$', views.settings, name='settings'),
+    # ex: /thermostats/5/temperature/
+    url(r'^(?P<thermostat_id>\d+)/temperature/$', views.temperature, name='temperature'),
 )
--- thermostats/views.py
+++ thermostats/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the thermostat index.")
+def detail(request, thermostat_id):
+    return HttpResponse("You're looking at thermostats %s." % thermostat_id)
+
+def settings(request, thermostat_id):
+    return HttpResponse("You're looking at the settings of thermostat %s." % thermostat_id)
+
+def temperature(request, thermostat_id):
+    return HttpResponse("You're temperature information on thermostat %s." % thermostat_id)
