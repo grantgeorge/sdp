4515953bca2914747ac3ed0945ea1b19a59354ee
GrantGeorge <grantelliottgeorge@gmail.com>
2013-11-06 21:44:31 -0500

    snippet app

--- cloudstat/urls.py
+++ cloudstat/urls.py
@@ -36,9 +36,10 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
     # url(r'^$', 'cloudstat.views.home', name='home'),
     url(r'^thermostats/', include('thermostats.urls', namespace="thermostats")),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('snippets.urls')), 
 )
