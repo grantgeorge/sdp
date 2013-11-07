67fb18434c92a27f823e7ba4c4e1d11ad270116c
GrantGeorge <grantelliottgeorge@gmail.com>
2013-11-06 20:56:27 -0500

    add REST

--- cloudstat/settings.py
+++ cloudstat/settings.py
@@ -130,7 +130,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'thermostats'
+    'thermostats',
+    'rest_framework'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
@@ -182,3 +183,16 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- thermostats/urls.py
+++ thermostats/urls.py
@@ -1,8 +1,26 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, routers
 
 from thermostats import views
 
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
+
+class GroupViewSet(viewsets.ModelViewSet):
+    model = Group
+
+# Routers provide an easy way of automatically determining the URL conf
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
 urlpatterns = patterns('',
+	# REST
+	url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # INDEX
     url(r'^$', views.index, name='index'),
     # ex: /thermostats/5/
     url(r'^(?P<thermostat_id>\d+)/$', views.detail, name='detail'),
@@ -10,4 +28,5 @@ urlpatterns = patterns('',
     url(r'^(?P<thermostat_id>\d+)/settings/$', views.settings, name='settings'),
     # ex: /thermostats/5/temperature/
     url(r'^(?P<thermostat_id>\d+)/temperature/$', views.temperature, name='temperature'),
+    
 )
