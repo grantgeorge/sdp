1596bf70e7535f8b9864e5e6ea5c85300d73a283
GrantGeorge <grantelliottgeorge@gmail.com>
2013-11-06 20:59:45 -0500

    add REST

--- thermostats/urls.py
+++ thermostats/urls.py
@@ -20,13 +20,6 @@ urlpatterns = patterns('',
 	# REST
 	url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # INDEX
-    url(r'^$', views.index, name='index'),
-    # ex: /thermostats/5/
-    url(r'^(?P<thermostat_id>\d+)/$', views.detail, name='detail'),
-    # ex: /thermostats/5/settings/
-    url(r'^(?P<thermostat_id>\d+)/settings/$', views.settings, name='settings'),
-    # ex: /thermostats/5/temperature/
-    url(r'^(?P<thermostat_id>\d+)/temperature/$', views.temperature, name='temperature'),
+
     
 )
