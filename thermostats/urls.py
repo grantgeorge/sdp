cc160c7c21c1e78456932a84fa06ad48a5bdd1d0
GrantGeorge <grantelliottgeorge@gmail.com>
2013-11-06 21:01:58 -0500

    wrong urls

--- thermostats/urls.py
+++ thermostats/urls.py
@@ -1,25 +1,13 @@
-from django.conf.urls import patterns, url, include
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, routers
+from django.conf.urls import patterns, url
 
 from thermostats import views
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    model = User
-
-class GroupViewSet(viewsets.ModelViewSet):
-    model = Group
-
-# Routers provide an easy way of automatically determining the URL conf
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
-
 urlpatterns = patterns('',
-	# REST
-	url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
-    
+    url(r'^$', views.index, name='index'),
+    # ex: /thermostats/5/
+    url(r'^(?P<thermostat_id>\d+)/$', views.detail, name='detail'),
+    # ex: /thermostats/5/settings/
+    url(r'^(?P<thermostat_id>\d+)/settings/$', views.settings, name='settings'),
+    # ex: /thermostats/5/temperature/
+    url(r'^(?P<thermostat_id>\d+)/temperature/$', views.temperature, name='temperature'),
 )
