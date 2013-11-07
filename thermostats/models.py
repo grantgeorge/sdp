efae01a29ac9e30da01ee7de596506b4caffe464
GrantGeorge <grantelliottgeorge@gmail.com>
2013-11-06 21:35:04 -0500

    snippets

--- thermostats/models.py
+++ thermostats/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
 class BaseControl(models.Model):
 	account_id = models.IntegerField(default=0)
@@ -26,3 +28,22 @@ class Thermostat(models.Model):
 		return self.name
 	def get_current_temperature(self):
 		return self.current_temperature
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES,
+                                default='python',
+                                max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES,
+                             default='friendly',
+                             max_length=100)
+
+    class Meta:
+        ordering = ('created',)
