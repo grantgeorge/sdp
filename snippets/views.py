e6346ac30abc6efebd5c991506a49314bb0a8646
GrantGeorge <grantelliottgeorge@gmail.com>
2013-11-19 08:49:16 -0500

    api tut 4

--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,29 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
-
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    def pre_save(self, obj):
+    	obj.owner = self.request.user
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    def pre_save(self, obj):
+    	obj.owner = self.request.user
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
