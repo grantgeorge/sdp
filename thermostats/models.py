88d8b80f50ffe27626120250bb8af35f1699e8d0
grant <grantelliottgeorge@gmail.com>
2013-12-03 18:05:26 -0500

    testing heroku push

--- thermostats/models.py
+++ thermostats/models.py
@@ -18,6 +18,8 @@ from pygments import highlight
 # 	def get_current_temperature(self):
 # 		return self.current_temperature
 
+# comment
+
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
