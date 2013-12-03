efeab0d7104ea819479855b56fc8a73c55a38e52
grant <grantelliottgeorge@gmail.com>
2013-12-03 18:13:09 -0500

    push to github

--- thermostats/models.py
+++ thermostats/models.py
@@ -18,7 +18,7 @@ from pygments import highlight
 # 	def get_current_temperature(self):
 # 		return self.current_temperature
 
-# comment
+# removed baseControl
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
